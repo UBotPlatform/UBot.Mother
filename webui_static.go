7820797624f94b032242193bfcc664e44c7fd91b
ArcticLampyrid <ArcticLampyrid@outlook.com>
2021-06-14 23:12:08 +0800

    fix: webui embed bug

--- webui_static.go
+++ webui_static.go
@@ -4,10 +4,14 @@ package main
 
 import (
 	"embed"
+	"io/fs"
 	"net/http"
 )
 
 //go:generate go run webui_static_generate.go
-//go:embed webui/build
+//go:embed webui/build/*
 var WebUIStaticAssets embed.FS
-var WebUIStaticHandler = http.FileServer(http.FS(WebUIStaticAssets))
+var WebUIStaticHandler = http.FileServer(http.FS(func() fs.FS {
+	r, _ := fs.Sub(WebUIStaticAssets, "webui/build")
+	return r
+}()))
